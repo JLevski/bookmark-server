25a3d3583c03a3dcb63f7e4abfc49cd78a705e08
JLevski <jsalevin@gmail.com>
2018-10-26 15:21:45 +1100

    setup for heroku

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -146,6 +146,7 @@ class Shortener(http.server.BaseHTTPRequestHandler):
             self.wfile.write("Couldn't fetch uri '{}'.".format(longuri).encode())
 
 if __name__ == '__main__':
-    server_address = ('', 8000)
+    port = int(os.environ.get('PORT', 8000))
+    server_address = ('', port)
     httpd = http.server.HTTPServer(server_address, Shortener)
     httpd.serve_forever()
